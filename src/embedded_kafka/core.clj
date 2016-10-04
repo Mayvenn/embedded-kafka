de82a1333a58ed66376885ee5a0e59225126a884
Greg Winn <gregw@mayvenn.com>
2016-10-04 13:35:50 -0700

    Fix to allow kafka offsets storage on one broker

--- src/embedded_kafka/core.clj
+++ src/embedded_kafka/core.clj
@@ -16,21 +16,22 @@
   (.getPath (apply file (System/getProperty "java.io.tmpdir") "embedded-kafka" parts)))
 
 (def ^:dynamic kafka-config
-  {"broker.id"                   "0"
-   "port"                        "9999"
-   "host.name"                   "localhost"
-   "metadata.broker.list"        "localhost:9999"
-   "zookeeper.connect"           "127.0.0.1:2182"
-   "zookeeper-port"              "2182"
-   "log.flush.interval.messages" "1"
-   "auto.create.topics.enable"   "true"
-   "group.id"                    "consumer"
-   "auto.offset.reset"           "smallest"
-   "serializer.class"            "kafka.serializer.StringEncoder",
-   "retry.backoff.ms"            "500"
-   "message.send.max.retries"    "5"
-   "auto.commit.enable"          "false"
-   "log.dir"                     (.getAbsolutePath (file (tmp-dir "kafka-log")))})
+  {"broker.id"                        "0"
+   "port"                             "9999"
+   "host.name"                        "localhost"
+   "metadata.broker.list"             "localhost:9999"
+   "zookeeper.connect"                "127.0.0.1:2182"
+   "zookeeper-port"                   "2182"
+   "offsets.topic.replication.factor" "1"
+   "log.flush.interval.messages"      "1"
+   "auto.create.topics.enable"        "true"
+   "group.id"                         "consumer"
+   "auto.offset.reset"                "smallest"
+   "serializer.class"                 "kafka.serializer.StringEncoder",
+   "retry.backoff.ms"                 "500"
+   "message.send.max.retries"         "5"
+   "auto.commit.enable"               "false"
+   "log.dir"                          (.getAbsolutePath (file (tmp-dir "kafka-log")))})
 
 (defn create-broker
   []
